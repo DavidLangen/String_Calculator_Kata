4d224390428a3e15d0d537cf4418f3e526cd9ed8
DavidLangen <pbg2h15aln@pb.bib.de>
2017-09-28 15:56:40 +0200

    Add StringCalculatorKata

---
+++ String_Calculator_Kata/StringCalculatorKata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Calculator_Kata
+{
+    class StringCalculatorKata
+    {
+        private List<String> _berechnungen;
+
+        public StringCalculatorKata()
+        {
+            _berechnungen = new List<String>();
+        }
+
+        public int calculator(String s)
+        {
+            
+
+            return 0;
+        }
+
+        private int minus()
+        {
+            return 0;
+        }
+
+        private int plus()
+        {
+            return 0;
+        }
+    }
+}
